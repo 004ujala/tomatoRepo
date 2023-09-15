e2047fa298ce3682415039fe3528c05bf8eea609
Ujala <004ujala@gmail.com>
2023-09-15 17:54:33 +0530

    adding parser to config file

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.DB_URL);
+        const conn = await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log('db connected successfully');
     } catch (error) {
         console.log(`error in db connecting`);
